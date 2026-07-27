79419a5c0c1bdb7e9e74f805038b0c48c52b197f
Sandeep Mahla <kaursandeep5705@gmail.com>
2026-07-27 23:43:20 +0530

    Create forEach-Set.js

---
+++ forEach-Set.js
@@ -0,0 +1,7 @@
+let rs = new Set(["oranges", "apples", "bananas"])
+rs.forEach((value, valueAgain, set) =>
+    {
+        console.log(value)
+        console.log(valueAgain)
+        console.log(set)
+    })
